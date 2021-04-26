0e2187f819b96d54f8095e064b91165349fdc67a
Anton Drugov <62794541+antikd8@users.noreply.github.com>
2021-04-27 01:05:21 +0300

    use approach 'File-First' for adding data to db

--- lab11/MainWindow.xaml.cs
+++ lab11/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lab11
 {
@@ -20,9 +23,32 @@ namespace lab11
     /// </summary>
     public partial class MainWindow : Window
     {
+        Context context = new Context();
         public MainWindow()
         {
             InitializeComponent();
+            using (Context db = new Context())
+            {
+
+                // создаем объекты Song
+            //    Author temp1 = new Author { Author_Name = "t1st", Age = 34 };
+
+
+                // добавляем объекты Song в контекст данных
+
+       //         db.Authors.Add(temp1);
+
+         //       db.SaveChanges();
+
+         //       Song temp = new Song { Id = 1, Name = "nice song", Author = temp1.Author_Name };
+
+         //       db.Songs.Add(temp);
+
+                // сохраняем контекст данных в базу данных
+        //        db.SaveChanges();
+            }
+            flatGrid.ItemsSource = (from item in context.Songs
+                                    select new { item.Id, item.Author, item.Name }).ToList();
         }
 
 
@@ -46,5 +72,28 @@ namespace lab11
             }
         }
 
+    }
+    [Table("Songs")]
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+    [Table("Authors")]
+    public class Author
+    {
+        [Key]
+        public string Author_Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Context : DbContext
+    {
+        public Context() : base("connectionDB") { }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
